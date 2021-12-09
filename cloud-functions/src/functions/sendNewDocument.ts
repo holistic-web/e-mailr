4dc52a7b6e3bc9375f8d035000349302df5021fc
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2021-12-09 02:29:17 +0000

    feat: use env for stripe key

--- cloud-functions/src/functions/sendNewDocument.ts
+++ cloud-functions/src/functions/sendNewDocument.ts
@@ -2,61 +2,66 @@ import * as functions from "firebase-functions";
 import { Document, DocumentStatus } from "../../../firestore/types";
 // import config from "../config";
 const admin = require("firebase-admin");
-// TODO: set up dev and live values for stripe secret key
-const stripe = require("stripe")("sk_test_51H5aXrBOHct1ZvxbphXmm0zC9G4IPcs3oYUCUhfxn7IsbzKHJMjAGWuPUHBjtiXMzoNO6eIwbTwOzIGo52H6AiNo00Z9g2nftu");
+const config = require('../config');
+const stripe = require("stripe")(config.stripe_secret_key);
 
 const sendNewDocument = async (data: any, context: any) => {
-  console.log('context: ', context);
-  console.log('data: ', data);
-  if (!context.auth)
-    throw new Error("you must be authenticated to call this function");
+  try {
+    console.log('context: ', context);
+    console.log('data: ', data);
+    if (!context.auth)
+      throw new Error("you must be authenticated to call this function");
 
-  // TODO: verify the recipient with the Stannp API
+    // TODO: verify the recipient with the Stannp API
 
-  const document: Document = {
-    userId: context.auth.uid,
-    textContent: data.textContent,
-    recipient: data.recipient,
-    status: DocumentStatus.DRAFT,
-  };
-  const documentsCollection = admin.firestore().collection("documents");
-  const documentRef = await documentsCollection.add(document);
-  const id = documentRef.id;
+    const document: Document = {
+      userId: context.auth.uid,
+      textContent: data.textContent,
+      recipient: data.recipient,
+      status: DocumentStatus.DRAFT,
+    };
+    const documentsCollection = admin.firestore().collection("documents");
+    const documentRef = await documentsCollection.add(document);
+    const id = documentRef.id;
 
-  // Create a stripe session and redirect the user to the payments page
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    customer_email: context.auth.token.email,
-    mode: "payment",
-    line_items: [
-      {
-        price_data: {
-          product_data: {
-            name: "Mail",
-            metadata: {
-              documnameentId: id
-            }
+    // Create a stripe session and redirect the user to the payments page
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      customer_email: context.auth.token.email,
+      mode: "payment",
+      line_items: [
+        {
+          price_data: {
+            product_data: {
+              name: "Mail",
+              metadata: {
+                documnameentId: id
+              }
+            },
+            currency: "gbp",
+            unit_amount: 200,
           },
-          currency: "gbp",
-          unit_amount: 200,
+          quantity: 1,
         },
-        quantity: 1,
-      },
-    ],
+      ],
 
-    // TODO: implement a page that handles the stripe redirect on completion
-    // TODO: how do we verify we were actually sent to this page by stripe?
-    success_url: "http://localhost:3000/success",
-    cancel_url: "http://localhost:3000/cancel",
-    // TODO: tidy up the success / cancel pages that already exist if we don't use them
-  });
-  console.log('session: ', session);
-  // Attach the stripe session id to the document in our database
-  await documentsCollection.doc(id).update({
-    stripeSessionId: session.id,
-  });
+      // TODO: implement a page that handles the stripe redirect on completion
+      // TODO: how do we verify we were actually sent to this page by stripe?
+      success_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/cancel",
+      // TODO: tidy up the success / cancel pages that already exist if we don't use them
+    });
+    console.log('session: ', session);
+    // Attach the stripe session id to the document in our database
+    await documentsCollection.doc(id).update({
+      stripeSessionId: session.id,
+    });
 
-  return { url: session.url };
+    return { url: session.url };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 export default functions.https.onCall(sendNewDocument);
